7a30c58abf7113866e16e6d1d1216e70f72777c2
Baptiste Devessier <baptiste@devessier.fr>
2023-11-16 16:34:14 +0100

    Add branchall (#13)
    * feat: add support for branchall nodes
    
    * chore: changesets

--- tests/flow.spec.ts
+++ tests/flow.spec.ts
@@ -72,6 +72,42 @@ test("Generate input, actions and conditions", ({ expect }) => {
           ],
         },
       },
+      {
+        id: "node-5",
+        type: "branchall",
+        data: {
+          branches: [{ id: "cond-5-6", summary: "First parallel branch" }, { id: "cond-5-7", summary: "Second parallel branch" }],
+          parallel: true
+        },
+      },
+      {
+        id: "node-6",
+        type: "action",
+        data: {
+          actionName: "Send email",
+          inputs: [
+            {
+              parameter: "email",
+              type: "javascript",
+              expr: "flow_input.agent_email",
+            },
+          ],
+        },
+      },
+      {
+        id: "node-7",
+        type: "action",
+        data: {
+          actionName: "Send email",
+          inputs: [
+            {
+              parameter: "email",
+              type: "javascript",
+              expr: "flow_input.agent_email",
+            },
+          ],
+        },
+      },
     ],
     edges: [
       {
@@ -80,15 +116,32 @@ test("Generate input, actions and conditions", ({ expect }) => {
         target: "node-2",
       },
       {
-        id: "edge-1",
+        id: "edge-2",
         source: "node-2",
         target: "node-3",
       },
       {
-        id: "edge-1",
+        id: "edge-3",
         source: "node-3",
         sourceHandle: "cond-3-1",
         target: "node-4",
+      },
+      {
+        id: "edge-4",
+        source: "node-4",
+        target: "node-5",
+      },
+      {
+        id: "edge-5",
+        source: "node-5",
+        sourceHandle: "cond-5-6",
+        target: "node-6",
+      },
+      {
+        id: "edge-6",
+        source: "node-5",
+        sourceHandle: "cond-5-7",
+        target: "node-7",
       },
     ],
   });
@@ -155,6 +208,53 @@ test("Generate input, actions and conditions", ({ expect }) => {
                           },
                           "path": "Send SMS",
                           "type": "script",
+                        },
+                      },
+                      {
+                        "id": "node-5",
+                        "value": {
+                          "branches": [
+                            {
+                              "modules": [
+                                {
+                                  "id": "node-6",
+                                  "value": {
+                                    "input_transforms": {
+                                      "email": {
+                                        "expr": "flow_input.agent_email",
+                                        "type": "javascript",
+                                      },
+                                    },
+                                    "path": "Send email",
+                                    "type": "script",
+                                  },
+                                },
+                              ],
+                              "skip_failure": undefined,
+                              "summary": "First parallel branch",
+                            },
+                            {
+                              "modules": [
+                                {
+                                  "id": "node-7",
+                                  "value": {
+                                    "input_transforms": {
+                                      "email": {
+                                        "expr": "flow_input.agent_email",
+                                        "type": "javascript",
+                                      },
+                                    },
+                                    "path": "Send email",
+                                    "type": "script",
+                                  },
+                                },
+                              ],
+                              "skip_failure": undefined,
+                              "summary": "Second parallel branch",
+                            },
+                          ],
+                          "parallel": true,
+                          "type": "branchall",
                         },
                       },
                     ],
